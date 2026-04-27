625dae953c4e0997bb95391ddf29356f03dab579
Harshit Srikoti <srikotiharshit9@gmail.com>
2026-04-27 23:27:13 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> freq;
+
+        
+
+        for(auto i : t)
+        {
+            freq[i]++;
+        }
+
+        int start = 0;
+        int end = 0;
+        int n = s.length();
+        int m = t.length();
+        int min = INT_MAX;
+
+        if(m > n)
+        {
+            return "";
+        }
+        
+
+        int index;
+
+        while(end < n)
+        {
+            char ch = s[end];
+            freq[ch]--;
+
+            if(freq[ch] >= 0) 
+            {
+                m--;
+            }
+
+            while(m == 0 && start <= end)
+            {
+                if(min > (end - start) + 1)
+                {
+                    min = end - start + 1;
+                    index = start;
+                }
+                freq[s[start]]++;
+                if(freq[s[start]] > 0)
+                {
+                    m++;
+                }
+                start++;
+            }
+            end++;
+        }
+
+        return (min == INT_MAX) ? "" : s.substr(index,min);
+    }
+};

8cde3ab65e040a8c3262c02a9d10d0bf73c01da7
Harshit Srikoti <srikotiharshit9@gmail.com>
2026-06-11 13:24:56 +0530

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode *slow = head;
+        ListNode * fast = head;
+
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+        ListNode *l2 = slow->next;
+        slow->next = nullptr;
+        ListNode *l1 = head;
+
+        ListNode *prev = NULL , *fut = NULL;
+        while(l2)
+        {
+            fut = l2->next;
+            l2->next = prev;
+            prev = l2;
+            l2 = fut;
+        }
+        l2 = prev;
+
+        ListNode* ans = new ListNode(0);
+        ListNode* temp = ans;
+        while(l2)
+        {
+            temp->next = l1;
+            l1 = l1->next;
+            temp = temp->next;
+            temp->next = nullptr;
+
+            temp->next = l2;
+            l2 = l2->next;
+            temp = temp->next;
+            temp->next = nullptr;
+        }
+        if(l1)
+        {
+            temp->next = l1;
+        }
+        temp = ans;
+        ans = temp->next;
+        delete(temp);
+
+        head = ans;
+    }
+};

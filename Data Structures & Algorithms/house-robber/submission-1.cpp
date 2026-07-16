c02ec642b11ef2396703f939d33bfd2709a1f2f2
Harshit Srikoti <srikotiharshit9@gmail.com>
2026-07-16 23:19:48 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    int maxAmt(int n , vector<int>& nums , vector<int>& dp) {
+        if(n == 0) return nums[0];
+        if(n == 1) {
+            return max(nums[0] , nums[1]);
+        }
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = max(nums[n] + maxAmt(n-2,nums,dp) , maxAmt(n-1,nums,dp));
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        return maxAmt(n-1,nums,dp);
+    }
+};
